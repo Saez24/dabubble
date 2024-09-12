4ef54c5b8f4b8b4452087542fef968b933f35a4b
Preston Jones <preston.jones@gmx.de>
2024-09-12 16:41:13 +0200

    [create] login header and card template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { CreateAccountComponent } from './login/create-account/create-account.component';
+import { ResetPasswordComponent } from './login/reset-password/reset-password.component';
+import { SignInComponent } from './login/sign-in/sign-in.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LoginComponent,
+        children: [
+            { path: '', component: SignInComponent },
+            { path: 'create-account', component: CreateAccountComponent },
+            { path: 'reset-password', component: ResetPasswordComponent },
+        ] },
+];
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
-
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
+import { CreateAccountComponent } from './create-account/create-account.component';
+import { SignInComponent } from "./sign-in/sign-in.component";
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonModule,
+    RouterModule,
+    CreateAccountComponent,
+    SignInComponent,
+    MatCardModule,
+    MatIconModule
+],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: [
+    './login.component.scss',
+    './../../styles.scss'
+  ]
 })
 export class LoginComponent {
+
+  ngOnInit() {
+  }
 
 }
