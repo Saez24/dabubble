aff32d69e4fdd03cab182f156425dd6d1850b909
Alex <saez0724+github@googlemail.com>
2024-09-12 15:22:13 +0200

    working on createaccount component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { SelectAvatarComponent } from './login/create-account/select-avatar/select-avatar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'selectavatar', component: SelectAvatarComponent },
+];
