3ee22909a7a0594eeb91aa425385fc8fbc33bf9f
Moritz Mayer-Eckardt <m.mayereckardt@gmail.com>
2024-09-12 17:28:41 +0200

    select avatar component

--- src/app/login/create-account/select-avatar/select-avatar.component.ts
+++ src/app/login/create-account/select-avatar/select-avatar.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { NgClass, NgFor, NgStyle } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-select-avatar',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, NgFor, MatButtonModule, NgClass, NgStyle, MatIconModule],
   templateUrl: './select-avatar.component.html',
   styleUrl: './select-avatar.component.scss'
 })
-export class SelectAvatarComponent {
+export class SelectAvatarComponent implements OnInit {
 
+  avatars = [
+    '../../../../assets/avatars/avatar1.svg',
+    '../../../../assets/avatars/avatar2.svg',
+    '../../../../assets/avatars/avatar3.svg',
+    '../../../../assets/avatars/avatar4.svg',
+    '../../../../assets/avatars/avatar5.svg',
+    '../../../../assets/avatars/avatar6.svg',
+  ];
+
+  selectedAvatar: string | null = null;
+  avatarSelected = false;
+
+  ngOnInit() {
+    this.shuffleAvatars();
+  }
+
+onAvatarClick(avatar: string) {
+  this.selectedAvatar = avatar;
+  this.avatarSelected = true;
+}
+
+  shuffleAvatars() {
+    for (let i = this.avatars.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.avatars[i], this.avatars[j]] = [this.avatars[j], this.avatars[i]];
+    }
+  }
 }
