8ac1e986839e95447326e09bb29f2122c79afa36
Rio741 <rio96@gmx.de>
2024-09-13 10:50:44 +0700

    reset-password-component

---
+++ src/app/login/reset-password/send-email/send-email.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null): boolean {
+    return !!(control && control.invalid && (control.dirty || control.touched));
+  }
+}
+
+@Component({
+  selector: 'app-send-email',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    ReactiveFormsModule
+  ],
+  templateUrl: './send-email.component.html',
+  styleUrls: ['./send-email.component.scss']
+})
+
+export class SendEmailComponent {
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  formSubmitted = false;
+  matcher: MyErrorStateMatcher = new MyErrorStateMatcher();
+
+  nextstep() {
+    this.formSubmitted = true;
+    if (this.emailFormControl.valid) {
+      console.log('Form is valid');
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+}
