808e5cbc58b01275b37e497817cd085b42cf47be
Alex <saez0724+github@googlemail.com>
2024-09-16 20:00:18 +0200

    working on chat-window-component

--- src/app/board/chat-window/chat-window.component.ts
+++ src/app/board/chat-window/chat-window.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-chat-window',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, MatIconModule, MatDividerModule, FormsModule, MatFormFieldModule, MatInputModule, CommonModule],
   templateUrl: './chat-window.component.html',
-  styleUrl: './chat-window.component.scss'
+  styleUrl: './chat-window.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None
 })
 export class ChatWindowComponent {
 
