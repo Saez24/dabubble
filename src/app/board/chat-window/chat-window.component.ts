84f3a589ad74a368d0ef8fa92f6a266b2278b6fd
Alex <saez0724+github@googlemail.com>
2024-09-16 21:19:35 +0200

    add emoji picker to chat-window-component

--- src/app/board/chat-window/chat-window.component.ts
+++ src/app/board/chat-window/chat-window.component.ts
@@ -6,18 +6,31 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgIf } from '@angular/common';
+import { PickerComponent } from '@ctrl/ngx-emoji-mart';
 
 
 @Component({
   selector: 'app-chat-window',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, MatIconModule, MatDividerModule, FormsModule, MatFormFieldModule, MatInputModule, CommonModule],
+  imports: [MatCardModule, MatButtonModule, MatIconModule, MatDividerModule, FormsModule,
+    MatFormFieldModule, MatInputModule, CommonModule, PickerComponent, NgIf],
   templateUrl: './chat-window.component.html',
   styleUrl: './chat-window.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None
 })
 export class ChatWindowComponent {
+  showEmojiPicker = false;
+  message = '';
 
+  showEmoji() {
+    this.showEmojiPicker = !this.showEmojiPicker;
+  }
+
+  addEmoji(event: any) {
+    this.message += event.emoji.native;
+    console.log(event.emoji.native);
+
+  }
 }
