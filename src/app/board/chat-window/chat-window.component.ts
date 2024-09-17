ba70a8937b234aad5943196ee9e36b6743f14d7f
Alex <saez0724+github@googlemail.com>
2024-09-17 12:25:51 +0200

    finished chat-window

--- src/app/board/chat-window/chat-window.component.ts
+++ src/app/board/chat-window/chat-window.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostListener, ViewEncapsulation } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -31,6 +31,14 @@ export class ChatWindowComponent {
   addEmoji(event: any) {
     this.message += event.emoji.native;
     console.log(event.emoji.native);
+  }
 
+  @HostListener('document:click', ['$event'])
+  clickOutside(event: Event) {
+    const target = event.target as HTMLElement;
+
+    if (this.showEmojiPicker && !target.closest('emoji-mart') && !target.closest('.message-icon')) {
+      this.showEmojiPicker = false;
+    }
   }
 }
