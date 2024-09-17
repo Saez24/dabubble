2833c7b99890cb8ccca207dd284b31ba70c56348
Moritz Mayer-Eckardt <m.mayereckardt@gmail.com>
2024-09-17 17:00:50 +0200

    edited dialog

--- src/app/board/chat-window/chat-window.component.ts
+++ src/app/board/chat-window/chat-window.component.ts
@@ -1,44 +1,44 @@
-// import { ChangeDetectionStrategy, Component, HostListener, ViewEncapsulation } from '@angular/core';
-// import { MatCardModule } from '@angular/material/card';
-// import { MatButtonModule } from '@angular/material/button';
-// import { MatIconModule } from '@angular/material/icon';
-// import { MatDividerModule } from '@angular/material/divider';
-// import { MatInputModule } from '@angular/material/input';
-// import { MatFormFieldModule } from '@angular/material/form-field';
-// import { FormsModule } from '@angular/forms';
-// import { CommonModule, NgIf } from '@angular/common';
-// import { PickerComponent } from '@ctrl/ngx-emoji-mart';
+import { ChangeDetectionStrategy, Component, HostListener, ViewEncapsulation } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { CommonModule, NgIf } from '@angular/common';
+import { PickerComponent } from '@ctrl/ngx-emoji-mart';
 
 
-// @Component({
-//   selector: 'app-chat-window',
-//   standalone: true,
-//   imports: [MatCardModule, MatButtonModule, MatIconModule, MatDividerModule, FormsModule,
-//     MatFormFieldModule, MatInputModule, CommonModule, PickerComponent, NgIf],
-//   templateUrl: './chat-window.component.html',
-//   styleUrl: './chat-window.component.scss',
-//   changeDetection: ChangeDetectionStrategy.OnPush,
-//   encapsulation: ViewEncapsulation.None
-// })
-// export class ChatWindowComponent {
-//   showEmojiPicker = false;
-//   message = '';
+@Component({
+  selector: 'app-chat-window',
+  standalone: true,
+  imports: [MatCardModule, MatButtonModule, MatIconModule, MatDividerModule, FormsModule,
+    MatFormFieldModule, MatInputModule, CommonModule, PickerComponent, NgIf],
+  templateUrl: './chat-window.component.html',
+  styleUrl: './chat-window.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None
+})
+export class ChatWindowComponent {
+  showEmojiPicker = false;
+  message = '';
 
-//   showEmoji() {
-//     this.showEmojiPicker = !this.showEmojiPicker;
-//   }
+  showEmoji() {
+    this.showEmojiPicker = !this.showEmojiPicker;
+  }
 
-//   addEmoji(event: any) {
-//     this.message += event.emoji.native;
-//     console.log(event.emoji.native);
-//   }
+  addEmoji(event: any) {
+    this.message += event.emoji.native;
+    console.log(event.emoji.native);
+  }
 
-//   @HostListener('document:click', ['$event'])
-//   clickOutside(event: Event) {
-//     const target = event.target as HTMLElement;
+  @HostListener('document:click', ['$event'])
+  clickOutside(event: Event) {
+    const target = event.target as HTMLElement;
 
-//     if (this.showEmojiPicker && !target.closest('emoji-mart') && !target.closest('.message-icon')) {
-//       this.showEmojiPicker = false;
-//     }
-//   }
-// }
+    if (this.showEmojiPicker && !target.closest('emoji-mart') && !target.closest('.message-icon')) {
+      this.showEmojiPicker = false;
+    }
+  }
+}
