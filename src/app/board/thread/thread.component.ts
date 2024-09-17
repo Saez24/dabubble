7d75cdd3b3e6110d10dc764d941220e19319228e
Rio741 <rio96@gmx.de>
2024-09-17 07:24:33 +0700

    styling-thread

--- src/app/board/thread/thread.component.ts
+++ src/app/board/thread/thread.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-thread',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, MatIconModule],
   templateUrl: './thread.component.html',
   styleUrl: './thread.component.scss'
 })
