9f94dffadbe5e8346f34c0e754b4e2c419bc641a
preston-jones <preston.jones@gmx.de>
2024-09-24 15:48:26 +0200

    changed id to uid in user.class.ts

--- src/app/shared/models/user.class.ts
+++ src/app/shared/models/user.class.ts
@@ -1,7 +1,7 @@
 type loginState = 'loggedIn' | 'loggedOut' | 'inactive';
 
 export class User {
-    id: string | null;
+    uid: string | null;
     email: string | null;
     name: string | null;
     avatarPath: string | null;
@@ -9,7 +9,7 @@ export class User {
     channels: string[] = [];
 
     constructor(obj?: any) {
-        this.id = obj ? obj.id: null;
+        this.uid = obj ? obj.id: null;
         this.email = obj ? obj.email: null;
         this.name = obj ? obj.name: null;
         this.avatarPath = obj ? obj.avatarPath: null;
